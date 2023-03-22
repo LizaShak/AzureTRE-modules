9e8d3c36d2fc6b75233be29702a5ab0b6f39fc00
LizaShak <iliza@outlook.com>
2023-03-22 10:33:26 +0200

    Cahnge validation

--- terraform_azurerm_environment_configuration/variables.tf
+++ terraform_azurerm_environment_configuration/variables.tf
@@ -2,7 +2,7 @@ variable "arm_environment" {
   type = string
 
   validation {
-    condition     = contains(["public", "usgovernment"], var.input_parameter)
+    condition     = contains(["public", "usgovernment"], var.arm_environment)
     error_message = "Allowed values for input_parameter are \"public\" or \"usgovernment\"."
   }
 
